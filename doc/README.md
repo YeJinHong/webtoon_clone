f94e538df2833caf8adc226aad3f04a14e22c50b
홍예진 <teepij@naver.com>
2023-03-29 22:17:52 +0900

    Initalize webtoon_api

---
+++ doc/README.md
@@ -0,0 +1,6 @@
+# TID (Today I Done)
+## 3월 29일
+- SpringBoot 프로젝트 생성
+	- Gradle 의존성 적용되지 않는 문제 해결
+	(SpringBoot 버전에 따른 Java 버전 문제)
+	- Spring Data JPA 적용 시 필요한 datasource 설정 작성 
---
+++ webtoon_api/src/main/java/com/example/webtoon_api/WebtoonApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtoon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonApiApplication.class, args);
+	}
+
+}
