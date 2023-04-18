e89025025d7e3723217ffb3bbfccd67258d787f4
HongYejin <33932851+YeJinHong@users.noreply.github.com>
2023-04-18 23:26:27 +0900

    Create SecurityConfig.java

---
+++ webtoon_api/src/main/java/com/example/webtoon_api/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint; // 401 error(Unauthorized, 인증 자격 증명 불가) 처리
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler; // 403 error(Forbidden, 권한 없음) 처리
+    private final JwtTokenProvider jwtTokenProvider; //JWT 토큰 생성 관리
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration(); //CORS 설정
+        config.setAllowCredentials(true); // 서버 응답시 json을 자바스크립트에서 처리 가능
+        config.addAllowedOriginPattern("*"); // 모든 ip 에 응답 허용
+        config.addAllowedHeader("*"); // 모든 header 응답 허용
+        config.addExposedHeader("*");
+        config.addAllowedMethod("*"); // 모든 요청 메소드 응답 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                    .disable() // 위조 인증 요청 방지
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //JWT 토큰 사용하기 때문에 session 사용 안함
+
+                .and()
+                .httpBasic()
+                    .disable() // HTTP Basic Authentication 사용 안함
+                .formLogin()
+                    .disable() // Form Based Authentication 사용 안함
+                .addFilter(corsFilter()) // 커스텀 필터(corsFilter) 추가
+                .exceptionHandling() // 예외처리 기능 사용
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 401 error(Unauthorized) 처리
+                .accessDeniedHandler(jwtAccessDeniedHandler) // 403 error(Forbidden) 처리
+                .and()
+                .apply(new JwtSecurityConfig(jwtTokenProvider)) // 사용자 정의 Filter(JWT 토큰 관련 처리) 추가
+
+                .and()
+                .authorizeHttpRequests() // 요청에 대한 권한 인증 필요
+                .requestMatchers().permitAll() //나머지 설정은 API 다 나오면 계속 진행 예정
+                .anyRequest().permitAll(); // 이외의 다른 요청들 모든 페이지 접근 가능
+
+        return http.build();
+    }
+
+}
+
