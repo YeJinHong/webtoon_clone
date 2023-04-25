cd93b4a2b5710e409a75d0015b9050837a7e5af0
YeJinHong <teepij@naver.com>
2023-04-25 22:27:58 +0900

    WebtoonController 주석처리. 프로젝트 뼈대 만든 후 다시 진행.

--- webtoon_api/src/main/java/com/example/webtoon_api/controller/WebtoonController.java
+++ webtoon_api/src/main/java/com/example/webtoon_api/controller/WebtoonController.java
@@ -1,5 +1,7 @@
 package com.example.webtoon_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/webtoons")
 public class WebtoonController {
     
-    @PostMapping(
-        path = ""
-    )
-    public BaseResponse<?> registerWebtoon(){
-    
-    }
-    
-    @GetMapping(
-        path = "/weekday/{weekday}"
-    )
-    public List<WebtoonResponseDTO> getWebtoonList(){
-    
-    }
+//    @PostMapping(
+//        path = ""
+//    )
+//    public BaseResponse<?> registerWebtoon(){
+//
+//    }
+//
+//    @GetMapping(
+//        path = "/weekday/{weekday}"
+//    )
+//    public List<WebtoonResponseDTO> getWebtoonList(){
+//
+//    }
 
 }
