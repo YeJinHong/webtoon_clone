9d140a4d2033beccba6c6c1617a532f5e3a90e0b
HongYejin <33932851+YeJinHong@users.noreply.github.com>
2023-05-06 22:56:21 +0900

    Update WebtoonController.java

--- webtoon_api/src/main/java/com/example/webtoon_api/controller/WebtoonController.java
+++ webtoon_api/src/main/java/com/example/webtoon_api/controller/WebtoonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/webtoons")
 public class WebtoonController {
-
+	// 웹툰 등록
 	// @PostMapping(
 	//     path = ""
 	// )
@@ -14,14 +14,17 @@ public class WebtoonController {
 	//
 	// }
 	//
+	// 어떤 요일의 웹툰 리스트
 	// @GetMapping(
 	//     path = "/weekday/{weekday}"
 	// )
 	// public List<WebtoonResponseDTO> getWebtoonList(){
 	//
 	// }
+	// 특정 웹툰 삭제
 	// @DeleteMapping
 	// public BaseResponse deleteWebtoon(@PathVariable String webtoonId){
 	//
 	// }
+	// 웹툰 정보 수정 (요일, 작가, 상태)
 }
